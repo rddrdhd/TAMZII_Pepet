e9efd6df3243927277568fcfb80fab8c9aa62202
Leona <zur0037@vsb.cz>
2019-10-22 14:55:50 +0200

    pet.setOnTouchListener()

--- app/src/main/java/com/example/thegathering/MainActivity.java
+++ app/src/main/java/com/example/thegathering/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.thegathering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView pet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pet = findViewById(R.id.imageViewPet);
+        pet.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                }
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                }
+                return true;
+            }
+        });
     }
 }
