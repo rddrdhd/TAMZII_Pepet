db16e84334afabfe6e5bda920b3e2a3e5255ae9d
Leona <zur0037@vsb.cz>
2019-11-20 19:22:41 +0100

    fullscreen activity

--- app/src/main/java/com/example/thegathering/MainActivity.java
+++ app/src/main/java/com/example/thegathering/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.thegathering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void fullscreenAct(View view){
+        Intent i = new Intent(this, FullscreenActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "!onActivityResult!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }//onActivityResult
 }
