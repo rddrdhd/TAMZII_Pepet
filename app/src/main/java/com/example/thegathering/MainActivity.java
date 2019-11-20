4f2b6a04d47d9be96fd30607bd22694abb18b5bd
Leona <zur0037@vsb.cz>
2019-11-20 20:22:17 +0100

    pet stats--

--- app/src/main/java/com/example/thegathering/MainActivity.java
+++ app/src/main/java/com/example/thegathering/MainActivity.java
@@ -4,16 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    Pet pet;
+    TextView tw1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tw1 = findViewById(R.id.textView);
+
+        tw1.setText("Happ: "+pet.statHappy+", Fed:"+pet.statFed+", Heal: "+pet.statHealth);
+
+        pet = new Pet();
+
+    }
+
+    Handler handler = new Handler();
+    Runnable runnable;
+    int delay = 15*1000; //Delay for 15 seconds.  One second = 1000 milliseconds.
+
+
+    @Override
+    protected void onResume() {
+        //start handler as activity become visible
+
+        handler.postDelayed( runnable = new Runnable() {
+            public void run() { //Every 15 seconds
+                //do something
+                pet.statHealth--;
+                pet.statFed--;
+                pet.statHappy--;
+
+
+                tw1.setText("Happ: "+pet.statHappy+", Fed:"+pet.statFed+", Heal: "+pet.statHealth);
+
+
+
+                Toast.makeText(getApplicationContext(), "stats refreshed", Toast.LENGTH_SHORT).show();
+                handler.postDelayed(runnable, delay);
+            }
+        }, delay);
+
+        super.onResume();
+    }
+
+// If onPause() is not included the threads will double up when you
+// reload the activity
+
+    @Override
+    protected void onPause() {
+        handler.removeCallbacks(runnable); //stop handler when activity not visible
+        super.onPause();
     }
 
     public void fullscreenAct(View view){
---
+++ app/src/main/java/com/example/thegathering/Pet.java
@@ -0,0 +1,21 @@
+package com.example.thegathering;
+
+public class Pet {
+    public String name;
+    public int age;
+
+    public int statHealth;
+    public int statHappy;
+    public int statFed;
+
+    public Pet(){
+        this.name = "Pet";
+        this.age = 0;
+        this.statHappy = 100;
+        this.statFed = 100;
+        this.statHealth = 100;
+    }
+
+
+
+}
