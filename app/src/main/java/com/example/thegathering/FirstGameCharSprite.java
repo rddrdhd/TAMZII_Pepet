ca7df82fcb3bd42de29fa92e2cc0654f5b82f7ea
Leona <zur0037@vsb.cz>
2019-11-24 15:20:47 +0100

    flappy pepe

--- app/src/main/java/com/example/thegathering/FirstGameCharSprite.java
+++ app/src/main/java/com/example/thegathering/FirstGameCharSprite.java
@@ -1,17 +1,13 @@
 package com.example.thegathering;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 
 public class FirstGameCharSprite {
     private Bitmap image;
     public int x, y;
     private int xVelocity = 10;
     public int yVelocity = 5;
-    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     public FirstGameCharSprite(Bitmap bmp){
         image = bmp;
         x = 100;
--- app/src/main/java/com/example/thegathering/FirstGameView.java
+++ app/src/main/java/com/example/thegathering/FirstGameView.java
@@ -2,6 +2,7 @@ package com.example.thegathering;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -11,29 +12,44 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
+import java.util.Random;
 
-public class FirstGameView extends android.view.SurfaceView implements android.view.SurfaceHolder.Callback  {
+
+public class FirstGameView
+        extends android.view.SurfaceView
+        implements android.view.SurfaceHolder.Callback  {
+
     private MainThread thread;
-    private FirstGameCharSprite charSprite;
+    private FirstGameCharSprite characterSprite;
+    public FirstGamePipeSprite pipe1, pipe2, pipe3;
+
+    public static int gapHeight = 500;
+    public static int velocity = 8;
+
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
 
     public FirstGameView(Context context) {
         super(context);
 
         getHolder().addCallback(this);
-
-
         thread = new MainThread(getHolder(), this);
         setFocusable(true);
     }
 
     public void update(){
-        charSprite.update();
+        logic();
+        characterSprite.update();
+        pipe1.update();
+        pipe2.update();
+        pipe3.update();
     }
 
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        charSprite.y = charSprite.y - (charSprite.yVelocity * 10);
+        characterSprite.y = characterSprite.y - (characterSprite.yVelocity * 10);
         return super.onTouchEvent(event);
     }
 
@@ -41,7 +57,23 @@ public class FirstGameView extends android.view.SurfaceView implements android.v
     public void surfaceCreated(SurfaceHolder holder) {
         thread.setRunning(true);
         thread.start();
-        charSprite = new FirstGameCharSprite(getResizedBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.wholepepe), 300, 300));
+        Bitmap bmp, bmp1, bmp2, resized, resized1, resized2;
+
+        int y;
+        int x;
+
+        bmp = BitmapFactory.decodeResource(getResources(),R.drawable.squarepepe);
+        bmp1 = BitmapFactory.decodeResource(getResources(), R.drawable.wall);
+        bmp2 = BitmapFactory.decodeResource(getResources(), R.drawable.wall);
+
+        resized =  getResizedBitmap( bmp, 250, 250);
+        resized1 =  getResizedBitmap( bmp1, 300, Resources.getSystem().getDisplayMetrics().heightPixels /-2);
+        resized2 =  getResizedBitmap( bmp2, 300, Resources.getSystem().getDisplayMetrics().heightPixels /2);
+
+        characterSprite = new FirstGameCharSprite(resized);
+        pipe1 = new FirstGamePipeSprite(resized1, resized2, 0, 0);
+        pipe2 = new FirstGamePipeSprite(resized1, resized2, 800, -250);
+        pipe3 = new FirstGamePipeSprite(resized1, resized2, 1600, 250);
     }
 
     @Override
@@ -73,7 +105,10 @@ public class FirstGameView extends android.view.SurfaceView implements android.v
            // Paint paint = new Paint();
            // paint.setColor(Color.rgb(250, 0, 0));
            // canvas.drawRect(100, 100, 200, 200, paint);
-            charSprite.draw(canvas);
+            characterSprite.draw(canvas);
+            pipe1.draw(canvas);
+            pipe2.draw(canvas);
+            pipe3.draw(canvas);
         }
     }
 
@@ -92,5 +127,79 @@ public class FirstGameView extends android.view.SurfaceView implements android.v
                 Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
         bm.recycle();
         return resizedBitmap;
+    }
+
+
+    public void logic() {
+
+        //Detect if the character is touching one of the pipes
+        if (characterSprite.y < pipe1.yY + (screenHeight / 2) - (gapHeight / 2)
+                && characterSprite.x + 300 > pipe1.xX && characterSprite.x < pipe1.xX + 500)
+        { resetLevel(); }
+
+        if (characterSprite.y < pipe2.yY + (screenHeight / 2) - (gapHeight / 2)
+                && characterSprite.x + 300 > pipe2.xX && characterSprite.x < pipe2.xX + 500)
+        { resetLevel(); }
+
+        if (characterSprite.y < pipe3.yY + (screenHeight / 2) - (gapHeight / 2)
+                && characterSprite.x + 300 > pipe3.xX && characterSprite.x < pipe3.xX + 500)
+        { resetLevel(); }
+
+        if (characterSprite.y + 240 > (screenHeight / 2) + (gapHeight / 2) + pipe1.yY
+                && characterSprite.x + 300 > pipe1.xX && characterSprite.x < pipe1.xX + 500)
+        { resetLevel(); }
+
+        if (characterSprite.y + 240 > (screenHeight / 2) + (gapHeight / 2) + pipe2.yY
+                && characterSprite.x + 300 > pipe2.xX && characterSprite.x < pipe2.xX + 500)
+        { resetLevel(); }
+
+        if (characterSprite.y + 240 > (screenHeight / 2) + (gapHeight / 2) + pipe3.yY
+                && characterSprite.x + 300 > pipe3.xX && characterSprite.x < pipe3.xX + 500)
+        { resetLevel(); }
+
+        //Detect if the character has gone off the
+        //bottom or top of the screen
+        if (characterSprite.y + 240 < 0) {
+            resetLevel(); }
+        if (characterSprite.y > screenHeight) {
+            resetLevel(); }
+
+
+        //If the pipe goes off the left of the screen,
+        //put it forward at a randomized distance and height
+        if (pipe1.xX + 500 < 0) {
+            Random r = new Random();
+            int value1 = r.nextInt(500);
+            int value2 = r.nextInt(500);
+            pipe1.xX = screenWidth + value1 + 1000;
+            pipe1.yY = value2 - 250;
+        }
+
+        if (pipe2.xX + 500 < 0) {
+            Random r = new Random();
+            int value1 = r.nextInt(500);
+            int value2 = r.nextInt(500);
+            pipe2.xX = screenWidth + value1 + 1000;
+            pipe2.yY = value2 - 250;
+        }
+
+        if (pipe3.xX + 500 < 0) {
+            Random r = new Random();
+            int value1 = r.nextInt(500);
+            int value2 = r.nextInt(500);
+            pipe3.xX = screenWidth + value1 + 1000;
+            pipe3.yY = value2 - 250;
+        }
+    }
+
+    public void resetLevel() {
+        characterSprite.y = 100;
+        pipe1.xX = 2000;
+        pipe1.yY = 0;
+        pipe2.xX = 4500;
+        pipe2.yY = 200;
+        pipe3.xX = 3200;
+        pipe3.yY = 250;
+
     }
 }
